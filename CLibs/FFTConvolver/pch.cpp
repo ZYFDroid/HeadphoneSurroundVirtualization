e790874e03adec221eb133c9808d4251cf98646e
ZYFDroid <zyfdroid@qq.com>
2022-07-13 19:21:46 +0800

    尝试将部分内存拷贝的部分扔进C++

---
+++ CLibs/FFTConvolver/pch.cpp
@@ -0,0 +1,350 @@
+﻿// pch.cpp: 与预编译标头对应的源文件
+
+#include "pch.h"
+using namespace fftconvolver;
+
+// 当使用预编译的头时，需要使用此源文件，编译才能成功。
+
+
+FFTConvolver* conv01 = new FFTConvolver();
+FFTConvolver* conv02 = new FFTConvolver();
+FFTConvolver* conv03 = new FFTConvolver();
+FFTConvolver* conv04 = new FFTConvolver();
+FFTConvolver* conv05 = new FFTConvolver();
+FFTConvolver* conv06 = new FFTConvolver();
+FFTConvolver* conv07 = new FFTConvolver();
+FFTConvolver* conv08 = new FFTConvolver();
+FFTConvolver* conv09 = new FFTConvolver();
+FFTConvolver* conv10 = new FFTConvolver();
+FFTConvolver* conv11 = new FFTConvolver();
+FFTConvolver* conv12 = new FFTConvolver();
+FFTConvolver* conv13 = new FFTConvolver();
+FFTConvolver* conv14 = new FFTConvolver();
+FFTConvolver* conv15 = new FFTConvolver();
+FFTConvolver* conv16 = new FFTConvolver();
+FFTConvolver* convOLL = new FFTConvolver();
+FFTConvolver* convOLR = new FFTConvolver();
+FFTConvolver* convORL = new FFTConvolver();
+FFTConvolver* convORR = new FFTConvolver();
+
+Sample* st1_FL_L = NULL;
+Sample* st1_FL_R = NULL;
+Sample* st1_FR_L = NULL;
+Sample* st1_FR_R = NULL;
+Sample* st1_FC_L = NULL;
+Sample* st1_FC_R = NULL;
+Sample* st1_LF_L = NULL;
+Sample* st1_LF_R = NULL;
+Sample* st1_RL_L = NULL;
+Sample* st1_RL_R = NULL;
+Sample* st1_RR_L = NULL;
+Sample* st1_RR_R = NULL;
+Sample* st1_SL_L = NULL;
+Sample* st1_SL_R = NULL;
+Sample* st1_SR_L = NULL;
+Sample* st1_SR_R = NULL;
+
+Sample* st2_L = NULL;
+Sample* st2_R = NULL;
+
+Sample* st3_L = NULL;
+Sample* st3_R = NULL;
+
+
+bool __stdcall con01_init(int blockSize, const fftconvolver::Sample* ir, int irLen)
+{
+    return conv01->init((size_t)blockSize, ir, (size_t)irLen);
+}
+
+void __stdcall con01_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len)
+{
+    conv01->process(input, (Sample*)output, (size_t)len);
+}
+
+void __stdcall con01_reset()
+{
+    conv01->reset();
+}
+bool __stdcall con02_init(int blockSize, const fftconvolver::Sample* ir, int irLen)
+{
+    return conv02->init((size_t)blockSize, ir, (size_t)irLen);
+}
+
+void __stdcall con02_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len)
+{
+    conv02->process(input, (Sample*)output, (size_t)len);
+}
+
+void __stdcall con02_reset()
+{
+    conv02->reset();
+}
+bool __stdcall con03_init(int blockSize, const fftconvolver::Sample* ir, int irLen)
+{
+    return conv03->init((size_t)blockSize, ir, (size_t)irLen);
+}
+
+void __stdcall con03_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len)
+{
+    conv03->process(input, (Sample*)output, (size_t)len);
+}
+
+void __stdcall con03_reset()
+{
+    conv03->reset();
+}
+bool __stdcall con04_init(int blockSize, const fftconvolver::Sample* ir, int irLen)
+{
+    return conv04->init((size_t)blockSize, ir, (size_t)irLen);
+}
+
+void __stdcall con04_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len)
+{
+    conv04->process(input, (Sample*)output, (size_t)len);
+}
+
+void __stdcall con04_reset()
+{
+    conv04->reset();
+}
+bool __stdcall con05_init(int blockSize, const fftconvolver::Sample* ir, int irLen)
+{
+    return conv05->init((size_t)blockSize, ir, (size_t)irLen);
+}
+
+void __stdcall con05_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len)
+{
+    conv05->process(input, (Sample*)output, (size_t)len);
+}
+
+void __stdcall con05_reset()
+{
+    conv05->reset();
+}
+bool __stdcall con06_init(int blockSize, const fftconvolver::Sample* ir, int irLen)
+{
+    return conv06->init((size_t)blockSize, ir, (size_t)irLen);
+}
+
+void __stdcall con06_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len)
+{
+    conv06->process(input, (Sample*)output, (size_t)len);
+}
+
+void __stdcall con06_reset()
+{
+    conv06->reset();
+}
+bool __stdcall con07_init(int blockSize, const fftconvolver::Sample* ir, int irLen)
+{
+    return conv07->init((size_t)blockSize, ir, (size_t)irLen);
+}
+
+void __stdcall con07_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len)
+{
+    conv07->process(input, (Sample*)output, (size_t)len);
+}
+
+void __stdcall con07_reset()
+{
+    conv07->reset();
+}
+bool __stdcall con08_init(int blockSize, const fftconvolver::Sample* ir, int irLen)
+{
+    return conv08->init((size_t)blockSize, ir, (size_t)irLen);
+}
+
+void __stdcall con08_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len)
+{
+    conv08->process(input, (Sample*)output, (size_t)len);
+}
+
+void __stdcall con08_reset()
+{
+    conv08->reset();
+}
+bool __stdcall con09_init(int blockSize, const fftconvolver::Sample* ir, int irLen)
+{
+    return conv09->init((size_t)blockSize, ir, (size_t)irLen);
+}
+
+void __stdcall con09_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len)
+{
+    conv09->process(input, (Sample*)output, (size_t)len);
+}
+
+void __stdcall con09_reset()
+{
+    conv09->reset();
+}
+bool __stdcall con10_init(int blockSize, const fftconvolver::Sample* ir, int irLen)
+{
+    return conv10->init((size_t)blockSize, ir, (size_t)irLen);
+}
+
+void __stdcall con10_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len)
+{
+    conv10->process(input, (Sample*)output, (size_t)len);
+}
+
+void __stdcall con10_reset()
+{
+    conv10->reset();
+}
+bool __stdcall con11_init(int blockSize, const fftconvolver::Sample* ir, int irLen)
+{
+    return conv11->init((size_t)blockSize, ir, (size_t)irLen);
+}
+
+void __stdcall con11_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len)
+{
+    conv11->process(input, (Sample*)output, (size_t)len);
+}
+
+void __stdcall con11_reset()
+{
+    conv11->reset();
+}
+bool __stdcall con12_init(int blockSize, const fftconvolver::Sample* ir, int irLen)
+{
+    return conv12->init((size_t)blockSize, ir, (size_t)irLen);
+}
+
+void __stdcall con12_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len)
+{
+    conv12->process(input, (Sample*)output, (size_t)len);
+}
+
+void __stdcall con12_reset()
+{
+    conv12->reset();
+}
+bool __stdcall con13_init(int blockSize, const fftconvolver::Sample* ir, int irLen)
+{
+    return conv13->init((size_t)blockSize, ir, (size_t)irLen);
+}
+
+void __stdcall con13_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len)
+{
+    conv13->process(input, (Sample*)output, (size_t)len);
+}
+
+void __stdcall con13_reset()
+{
+    conv13->reset();
+}
+bool __stdcall con14_init(int blockSize, const fftconvolver::Sample* ir, int irLen)
+{
+    return conv14->init((size_t)blockSize, ir, (size_t)irLen);
+}
+
+void __stdcall con14_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len)
+{
+    conv14->process(input, (Sample*)output, (size_t)len);
+}
+
+void __stdcall con14_reset()
+{
+    conv14->reset();
+}
+bool __stdcall con15_init(int blockSize, const fftconvolver::Sample* ir, int irLen)
+{
+    return conv15->init((size_t)blockSize, ir, (size_t)irLen);
+}
+
+void __stdcall con15_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len)
+{
+    conv15->process(input, (Sample*)output, (size_t)len);
+}
+
+void __stdcall con15_reset()
+{
+    conv15->reset();
+}
+bool __stdcall con16_init(int blockSize, const fftconvolver::Sample* ir, int irLen)
+{
+    return conv16->init((size_t)blockSize, ir, (size_t)irLen);
+}
+
+void __stdcall con16_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len)
+{
+    conv16->process(input, (Sample*)output, (size_t)len);
+}
+
+void __stdcall con16_reset()
+{
+    conv16->reset();
+}
+// begin of stage2
+bool __stdcall conOLL_init(int blockSize, const fftconvolver::Sample* ir, int irLen)
+{
+    return convOLL->init((size_t)blockSize, ir, (size_t)irLen);
+}
+
+void __stdcall conOLL_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len)
+{
+    convOLL->process(input, (Sample*)output, (size_t)len);
+}
+
+void __stdcall conOLL_reset()
+{
+    convOLL->reset();
+}
+
+bool __stdcall conOLR_init(int blockSize, const fftconvolver::Sample* ir, int irLen)
+{
+    return convOLR->init((size_t)blockSize, ir, (size_t)irLen);
+}
+
+void __stdcall conOLR_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len)
+{
+    convOLR->process(input, (Sample*)output, (size_t)len);
+}
+
+void __stdcall conOLR_reset()
+{
+    convOLR->reset();
+}
+
+
+
+
+bool __stdcall conORL_init(int blockSize, const fftconvolver::Sample* ir, int irLen)
+{
+    return convORL->init((size_t)blockSize, ir, (size_t)irLen);
+}
+
+void __stdcall conORL_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len)
+{
+    convORL->process(input, (Sample*)output, (size_t)len);
+}
+
+void __stdcall conORL_reset()
+{
+    convORL->reset();
+}
+
+bool __stdcall conORR_init(int blockSize, const fftconvolver::Sample* ir, int irLen)
+{
+    return convORR->init((size_t)blockSize, ir, (size_t)irLen);
+}
+
+void __stdcall conORR_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len)
+{
+    convORR->process(input, (Sample*)output, (size_t)len);
+}
+
+void __stdcall conORR_reset()
+{
+    convORR->reset();
+}
+
+
+bool __stdcall init_mem()
+{
+    bool result = true;
+
+    result &= (st1_FL_L = (fftconvolver::Sample *)malloc(sizeof(float) * 48000)) != 0;
+
+    return result;
+}
---
+++ CLibs/FFTConvolver/pch.h
@@ -0,0 +1,94 @@
+﻿// pch.h: 这是预编译标头文件。
+// 下方列出的文件仅编译一次，提高了将来生成的生成性能。
+// 这还将影响 IntelliSense 性能，包括代码完成和许多代码浏览功能。
+// 但是，如果此处列出的文件中的任何一个在生成之间有更新，它们全部都将被重新编译。
+// 请勿在此处添加要频繁更新的文件，这将使得性能优势无效。
+
+#ifndef PCH_H
+#define PCH_H
+
+#include <iostream>
+
+
+// 添加要在此处预编译的标头
+#include "framework.h"
+#include "src/AudioFFT.h"
+#include "src/FFTConvolver.h"
+#include "src/TwoStageFFTConvolver.h"
+#include "src/Utilities.h"
+
+#endif //PCH_H
+
+
+#ifdef IMPORT_DLL
+#else
+#define IMPORT_DLL extern "C" _declspec(dllimport) //指的是允许将其给外部调用
+#endif
+
+IMPORT_DLL bool __stdcall con01_init(int blockSize, const fftconvolver::Sample* ir, int irLen);
+IMPORT_DLL void __stdcall con01_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len);
+IMPORT_DLL void __stdcall con01_reset();
+IMPORT_DLL bool __stdcall con02_init(int blockSize, const fftconvolver::Sample* ir, int irLen);
+IMPORT_DLL void __stdcall con02_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len);
+IMPORT_DLL void __stdcall con02_reset();
+IMPORT_DLL bool __stdcall con03_init(int blockSize, const fftconvolver::Sample* ir, int irLen);
+IMPORT_DLL void __stdcall con03_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len);
+IMPORT_DLL void __stdcall con03_reset();
+IMPORT_DLL bool __stdcall con04_init(int blockSize, const fftconvolver::Sample* ir, int irLen);
+IMPORT_DLL void __stdcall con04_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len);
+IMPORT_DLL void __stdcall con04_reset();
+IMPORT_DLL bool __stdcall con05_init(int blockSize, const fftconvolver::Sample* ir, int irLen);
+IMPORT_DLL void __stdcall con05_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len);
+IMPORT_DLL void __stdcall con05_reset();
+IMPORT_DLL bool __stdcall con06_init(int blockSize, const fftconvolver::Sample* ir, int irLen);
+IMPORT_DLL void __stdcall con06_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len);
+IMPORT_DLL void __stdcall con06_reset();
+IMPORT_DLL bool __stdcall con07_init(int blockSize, const fftconvolver::Sample* ir, int irLen);
+IMPORT_DLL void __stdcall con07_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len);
+IMPORT_DLL void __stdcall con07_reset();
+IMPORT_DLL bool __stdcall con08_init(int blockSize, const fftconvolver::Sample* ir, int irLen);
+IMPORT_DLL void __stdcall con08_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len);
+IMPORT_DLL void __stdcall con08_reset();
+IMPORT_DLL bool __stdcall con09_init(int blockSize, const fftconvolver::Sample* ir, int irLen);
+IMPORT_DLL void __stdcall con09_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len);
+IMPORT_DLL void __stdcall con09_reset();
+IMPORT_DLL bool __stdcall con10_init(int blockSize, const fftconvolver::Sample* ir, int irLen);
+IMPORT_DLL void __stdcall con10_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len);
+IMPORT_DLL void __stdcall con10_reset();
+IMPORT_DLL bool __stdcall con11_init(int blockSize, const fftconvolver::Sample* ir, int irLen);
+IMPORT_DLL void __stdcall con11_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len);
+IMPORT_DLL void __stdcall con11_reset();
+IMPORT_DLL bool __stdcall con12_init(int blockSize, const fftconvolver::Sample* ir, int irLen);
+IMPORT_DLL void __stdcall con12_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len);
+IMPORT_DLL void __stdcall con12_reset();
+IMPORT_DLL bool __stdcall con13_init(int blockSize, const fftconvolver::Sample* ir, int irLen);
+IMPORT_DLL void __stdcall con13_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len);
+IMPORT_DLL void __stdcall con13_reset();
+IMPORT_DLL bool __stdcall con14_init(int blockSize, const fftconvolver::Sample* ir, int irLen);
+IMPORT_DLL void __stdcall con14_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len);
+IMPORT_DLL void __stdcall con14_reset();
+IMPORT_DLL bool __stdcall con15_init(int blockSize, const fftconvolver::Sample* ir, int irLen);
+IMPORT_DLL void __stdcall con15_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len);
+IMPORT_DLL void __stdcall con15_reset();
+IMPORT_DLL bool __stdcall con16_init(int blockSize, const fftconvolver::Sample* ir, int irLen);
+IMPORT_DLL void __stdcall con16_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len);
+IMPORT_DLL void __stdcall con16_reset();
+IMPORT_DLL bool __stdcall conOLL_init(int blockSize, const fftconvolver::Sample* ir, int irLen);
+IMPORT_DLL void __stdcall conOLL_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len);
+IMPORT_DLL void __stdcall conOLL_reset();
+IMPORT_DLL bool __stdcall conORL_init(int blockSize, const fftconvolver::Sample* ir, int irLen);
+IMPORT_DLL void __stdcall conORL_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len);
+IMPORT_DLL void __stdcall conORL_reset();
+
+IMPORT_DLL bool __stdcall conOLR_init(int blockSize, const fftconvolver::Sample* ir, int irLen);
+IMPORT_DLL void __stdcall conOLR_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len);
+IMPORT_DLL void __stdcall conOLR_reset();
+IMPORT_DLL bool __stdcall conORR_init(int blockSize, const fftconvolver::Sample* ir, int irLen);
+IMPORT_DLL void __stdcall conORR_process(const fftconvolver::Sample* input, const fftconvolver::Sample* output, int len);
+IMPORT_DLL void __stdcall conORR_reset();
+
+
+IMPORT_DLL bool __stdcall init_mem();
+
+
+
